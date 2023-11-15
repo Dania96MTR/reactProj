568a9f099b0889b3594f12eb1ac5874f89babe40
Dania96MTR <144219920+Dania96MTR@users.noreply.github.com>
2023-11-15 23:26:49 +0200

    react project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,40 @@
 import './App.css';
 import styles from "./App.module.css"
+import { useState } from 'react';
+
 
 function App() {
+  const [number , setInvertal] = useState("")
+  const [meters , setMeters] = useState("")
+
+ 
+  function change(e){
+    setInvertal(e.target.value)
+  }
+  function calc(e){
+    e.preventDefault()
+    const convert = parseInt(number) * 0.0254
+    setMeters(convert) 
+    
+  }
+
+  
   return (
-    <div>
-      <h1 className={styles.pageHeading}>Dania Matar</h1>
+    <>
+    <div className={styles.main}>
+        <p className={styles.inchToMeter}>Inch-To-Meter Converter</p>
+        <form onSubmit={(e) => calc(e)} >
+            <div>
+                <p className={styles.enterNumber}>Number of inches: </p>
+                <input onChange={(e) => change(e)} type="number" placeholder="Enter number of inches here.." id="inches" value={parseInt(number)} required ></input>
+            </div>
+            <button type="submit">Calculate</button>
+        </form>
+        <div className={styles.result}>
+           <p >{meters ? meters : "200 Inches = 5.08 meters"}</p> 
+        </div>
     </div>
+    </>
   );
 }
 
